d2e43c718f47b7c17fa8afbb69609ade0b423653
chikwongyip <527439024@qq.com>
2022-07-25 15:35:29 +0800

    常用内置函数

---
+++ 常用内置函数.py
@@ -0,0 +1,23 @@
+a = abs(-1.2)
+print(a)
+b = abs(3+4j)
+print(b)
+
+c = divmod(12,5)
+print(c)
+
+d = pow(2,5)
+print(d)
+
+e = round(12.54)
+print(e)
+
+f = round(12.54,1)
+print(f)
+
+g = round(12.567,2)
+print(g)
+
+print(max(1,2,3))
+
+print(min(1,2,3))
