dc6fe1c38a111db7e89a2b2dacce0f709c86223c
chikwong <527439024@qq.com>
2024-04-28 15:47:23 +0800

    类的学习

--- cls.py
+++ cls.py
@@ -1,3 +1,4 @@
+import math
 class leaf(object):
     """A leaf object"""
 
@@ -34,7 +35,39 @@ class Student:
 
     def __repr__(self):
         return f'Student(name={self.name}, age={self.age})'
+# 定义一个钟的类
+class Clock(object):
+    # 钟属性分别有时分秒
+    def __init__(self,second,minute,hour):
+        self.second = second
+        self.minute = minute
+        self.hour = hour
+    
+    # 时钟他自己会走
+    def run(self):
+        self.second = self.second + 1
+        if self.second == 60:
+            self.minute = self.minute + 1.
+            self.second = 0
+            if self.minute == 60:
+                self.hour = self.hour + 1
+                self.minute = 0
+                if self.hour == 24:
+                    self.hour = 0
+    def show(self):
+        print(f'{self.hour}:{self.minute}:{self.second}')
 
+# 屏幕点描述类
 
-stu1 = Student(1, 'happy', '40')
-print(stu1)
+class Point(object):
+    # 平面点有 x坐标和y坐标属性
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def show(self):
+        print(f'该点的x坐标为{self.x},y坐标为{self.y}')
+    
+    def show_distance(self,des):
+        # 两个值得绝对值
+        return (abs(self.x - des.x) + abs(self.y -des.y)) ** 0.5
